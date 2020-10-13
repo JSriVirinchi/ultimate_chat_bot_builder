7c85ee4dc039dd6abe40bf189d9f1a2e50167605
JSriVirinchi <jsrivirinchi@gmail.com>
2020-10-13 17:07:05 +0530

    home-intro page styling and js done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
 	def welcome
 	end
+
+	def createbot_intro
+	end
+
+	def selectbot
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#welcome'
+  get '/createbot/intro', to: 'home#createbot_intro'
+  get '/selectbot', to: 'home#selectbot'
 end
